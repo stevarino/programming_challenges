3dd59f3687702c6234abbff3f6f2705aace652d0
stevarino <s.j.oconnor@outlook.com>
2017-02-17 14:45:46 -0500

    Added project descriptions.

--- euler/challenge_10.py
+++ euler/challenge_10.py
@@ -1,4 +1,10 @@
-"""https://projecteuler.net/problem=10"""
+"""https://projecteuler.net/problem=10
+
+The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+
+Find the sum of all the primes below two million.
+
+"""
 
 def gen_primes(limit=2e6):
     """Generates primes, based on Sieve of Eratosthenes
